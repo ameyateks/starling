499a2f3db92e940a49a3ba0a97ffc4086ec01f92
Ameya <ameyatekale@gmail.com>
2024-04-16 23:37:35 +0100

    setting frontend url to read from env

--- backend/go.mod
+++ backend/go.mod
@@ -5,10 +5,10 @@ go 1.21
 toolchain go1.21.3
 
 require (
-	github.com/golang-migrate/migrate v3.5.4+incompatible
 	github.com/golang-migrate/migrate/v4 v4.17.0
 	github.com/gorilla/mux v1.8.1
 	github.com/jmoiron/sqlx v1.3.5
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 )
 
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
 )
 
 func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	(*w).Header().Set("Access-Control-Allow-Origin", os.Getenv("FRONTEND_URL"))
 	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept")
 }
